29dd8086723944a3fc0730c9c8ffc7a0b60dfab1
Dylan Meissner <dylanmei@gmail.com>
2015-01-01 15:53:31 -0800

    Can copy directories

--- winrmfs/winrmfs.go
+++ winrmfs/winrmfs.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/masterzen/winrm/winrm"
 )
@@ -22,13 +23,27 @@ func (fs *Winrmfs) Info() (*Info, error) {
 }
 
 func (fs *Winrmfs) Copy(fromPath, toPath string) error {
-	file, err := os.Open(fromPath)
+	f, err := os.Open(fromPath)
 	if err != nil {
-		return errors.New(fmt.Sprintf("Couldn't open file %s: %v", fromPath, err))
+		return errors.New(fmt.Sprintf("Couldn't read file %s: %v", fromPath, err))
 	}
 
-	defer file.Close()
-	return fs.Write(toPath, file)
+	defer f.Close()
+	fi, err := f.Stat()
+	if err != nil {
+		return errors.New(fmt.Sprintf("Couldn't stat file %s: %v", fromPath, err))
+	}
+
+	if !fi.IsDir() {
+		return fs.Write(toPath, f)
+	} else {
+		fw := fileWalker{
+			client:  fs.client,
+			toDir:   toPath,
+			fromDir: fromPath,
+		}
+		return filepath.Walk(fromPath, fw.copyFile)
+	}
 }
 
 func (fs *Winrmfs) Write(toPath string, src io.Reader) error {
@@ -37,4 +52,35 @@ func (fs *Winrmfs) Write(toPath string, src io.Reader) error {
 
 func (fs *Winrmfs) List(remotePath string) ([]FileItem, error) {
 	return fetchList(fs.client, winPath(remotePath))
+}
+
+type fileWalker struct {
+	client  *winrm.Client
+	toDir   string
+	fromDir string
+}
+
+func (fw *fileWalker) copyFile(fromPath string, fi os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if !shouldUploadFile(fi) {
+		return nil
+	}
+
+	relPath := filepath.Dir(fromPath[len(fw.toDir):len(fromPath)])
+	toPath := filepath.Join(fw.toDir, relPath, fi.Name())
+
+	f, err := os.Open(fromPath)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Couldn't read file %s: %v", fromPath, err))
+	}
+
+	return doCopy(fw.client, f, winPath(toPath))
+}
+
+func shouldUploadFile(fi os.FileInfo) bool {
+	// Ignore dir entries and OS X special hidden file
+	return !fi.IsDir() && ".DS_Store" != fi.Name()
 }
