258a16ef8ee9b931bc1f0b51e388cb8ca962b299
Matt Fellows <matt.fellows@onegeek.com.au>
2015-01-06 23:03:50 +1100

    Using absolute file references to calculate relative upload paths. Addresses issues #1 and #2

--- winrmcp/winrmcp.go
+++ winrmcp/winrmcp.go
@@ -69,10 +69,12 @@ func (fw *fileWalker) copyFile(fromPath string, fi os.FileInfo, err error) error
 		return nil
 	}
 
-	relPath := filepath.Dir(fromPath[len(fw.toDir):len(fromPath)])
-	toPath := filepath.Join(fw.toDir, relPath, fi.Name())
+	hostPath, _ := filepath.Abs(fromPath)
+	fromDir, _ := filepath.Abs(fw.fromDir)
+	relPath, _ := filepath.Rel(fromDir, hostPath)
+	toPath := filepath.Join(fw.toDir, relPath)
 
-	f, err := os.Open(fromPath)
+	f, err := os.Open(hostPath)
 	if err != nil {
 		return errors.New(fmt.Sprintf("Couldn't read file %s: %v", fromPath, err))
 	}
