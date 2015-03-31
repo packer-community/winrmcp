2c6c6803e19246dab106242b2a00fa12d8e1cb3b
James Nugent <james@jen20.com>
2015-03-30 21:30:43 -0400

    Add support for winrmcp over WinRM/HTTPS
    This commit adds support for using HTTPS listeners with winrmcp. It also
    adds flags to ignore validating the certificate chain, and for providing
    a custom root CA to validate against.

--- winrmcp/endpoint_test.go
+++ winrmcp/endpoint_test.go
@@ -1,11 +1,9 @@
 package winrmcp
 
-import (
-	"testing"
-)
+import "testing"
 
 func Test_parsing_an_addr_to_a_winrm_endpoint(t *testing.T) {
-	endpoint, err := parseEndpoint("1.2.3.4:1234")
+	endpoint, err := parseEndpoint("1.2.3.4:1234", false, false, nil)
 
 	if err != nil {
 		t.Fatalf("Should not have been an error: %v", err)
@@ -19,10 +17,17 @@ func Test_parsing_an_addr_to_a_winrm_endpoint(t *testing.T) {
 	if endpoint.Port != 1234 {
 		t.Error("Port should be 1234")
 	}
+	if endpoint.Insecure {
+		t.Error("Endpoint should be insecure")
+	}
+	if endpoint.HTTPS {
+		t.Error("Endpoint should be HTTP not HTTPS")
+	}
 }
 
 func Test_parsing_an_addr_without_a_port_to_a_winrm_endpoint(t *testing.T) {
-	endpoint, err := parseEndpoint("1.2.3.4")
+	certBytes := []byte{1, 2, 3, 4, 5, 6}
+	endpoint, err := parseEndpoint("1.2.3.4", true, true, certBytes)
 
 	if err != nil {
 		t.Fatalf("Should not have been an error: %v", err)
@@ -36,10 +41,25 @@ func Test_parsing_an_addr_without_a_port_to_a_winrm_endpoint(t *testing.T) {
 	if endpoint.Port != 5985 {
 		t.Error("Port should be 5985")
 	}
+	if endpoint.Insecure != true {
+		t.Error("Endpoint should be insecure")
+	}
+	if endpoint.HTTPS != true {
+		t.Error("Endpoint should be HTTPS")
+	}
+
+	if len(*endpoint.CACert) != len(certBytes) {
+		t.Error("Length of CACert is wrong")
+	}
+	for i := 0; i < len(certBytes); i++ {
+		if (*endpoint.CACert)[i] != certBytes[i] {
+			t.Error("CACert is not set correctly")
+		}
+	}
 }
 
 func Test_parsing_an_empty_addr_to_a_winrm_endpoint(t *testing.T) {
-	endpoint, err := parseEndpoint("")
+	endpoint, err := parseEndpoint("", false, false, nil)
 
 	if endpoint != nil {
 		t.Error("Endpoint should be nil")
@@ -50,7 +70,7 @@ func Test_parsing_an_empty_addr_to_a_winrm_endpoint(t *testing.T) {
 }
 
 func Test_parsing_an_addr_with_a_bad_port(t *testing.T) {
-	endpoint, err := parseEndpoint("1.2.3.4:ABCD")
+	endpoint, err := parseEndpoint("1.2.3.4:ABCD", false, false, nil)
 
 	if endpoint != nil {
 		t.Error("Endpoint should be nil")
